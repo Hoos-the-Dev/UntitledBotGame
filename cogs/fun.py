ef017dd9f90f9074c8a79042438d55aebbae0905
Aurora Rpg <supadj223@gmail.com>
2022-03-05 20:30:10 +0000

    more cogs

---
+++ cogs/fun.py
@@ -0,0 +1,44 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction
+
+class Fun(commands.Cog):
+  def __init__(self, bot):
+      self.bot = bot
+
+  testserverid = 937841015648292934
+  
+  @nextcord.slash_command(name ="dm", description = "DM other users with the bot", guild_ids=[testserverid])
+  async def dm(self, interaction: Interaction, ctx, user: nextcord.User, *, message=None, amount=1):
+    if message == None:
+        await interaction.response.send_message('You need to put a message')
+    else:
+        embed = nextcord.Embed(title="You got mail :incoming_envelope:", description=message)
+        embed.add_field(name="Sent by", value=ctx.author)
+        await user.send(embed=embed)
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'Delivered to {user}', delete_after=6)
+        
+  @nextcord.slash_command(name ="test", description = "testing slash commands", guild_ids=[testserverid])
+  async def test(self, interaction: Interaction):
+    await interaction.response.send_message('Je mappelle Bot')
+  
+  @commands.command()
+  async def anondm(self, ctx, user: nextcord.User, *, message=None, amount=1):
+    if message == None:
+        await ctx.send('You need to put a message')
+    else:
+        embed = nextcord.Embed(title="You got mail :incoming_envelope:", description=message)
+        embed.add_field(name="Sent by", value="Bingus")
+        await user.send(embed=embed)
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'Delivered to {user}', delete_after=6)
+
+  @commands.command()
+  async def ping(self, ctx):
+    await ctx.reply(":ping_pong: pong")
+    print("user has pinged")
+
+    
+def setup(bot):
+    bot.add_cog(Fun(bot))
