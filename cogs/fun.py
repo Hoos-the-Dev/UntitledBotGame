3827c09ca18389be8b48f17489819811abd23fdd
Aurora Rpg <supadj223@gmail.com>
2022-03-21 13:39:06 +0000

    moved the check

--- cogs/fun.py
+++ cogs/fun.py
@@ -20,17 +20,6 @@ class Fun(commands.Cog):
         await ctx.channel.purge(limit=1)
         await ctx.send(f'Delivered to {user}', delete_after=6)
   
-  
-  @dm.error
-  async def dm_error(self, ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-      await ctx.reply("You need to put a message to send. but you should've known that already dumbass")
-  
-  
-  @dm.error
-  async def validuser_error(self, ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-      await ctx.reply("You need to say a valid user to dm. but i thought that was obvious")
           
   @commands.command()
   @commands.guild_only()
@@ -45,6 +34,17 @@ class Fun(commands.Cog):
         await ctx.send(f'Delivered to {user}', delete_after=6)
       
 
+  @dm.error
+  async def dm_error(self, ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+      await ctx.reply("You need to put a message to send. but you should've known that already dumbass")
+  
+  
+  @dm.error
+  async def validuser_error(self, ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+      await ctx.reply("You need to say a valid user to dm. but i thought that was obvious")
+      
   @commands.command()
   async def feedfish(self, ctx):
     embed = nextcord.Embed(title="Yummy :fish:", description="You fed bingus some fish and Bingus starts to pur.")
