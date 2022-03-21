56e88a04d733e995f1b0594003ebdbc3da0b879b
Aurora Rpg <supadj223@gmail.com>
2022-03-21 13:40:21 +0000

    fixed the checks

--- cogs/fun.py
+++ cogs/fun.py
@@ -20,6 +20,17 @@ class Fun(commands.Cog):
         await ctx.channel.purge(limit=1)
         await ctx.send(f'Delivered to {user}', delete_after=6)
   
+  
+  @dm.error
+  async def dm_error(self, ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+      await ctx.reply("You need to put a message to send. but you should've known that already dumbass")
+  
+  
+  @dm.error
+  async def validuser_error(self, ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+      await ctx.reply("You need to say a valid user to dm. but i thought that was obvious")
           
   @commands.command()
   @commands.guild_only()
@@ -32,19 +43,18 @@ class Fun(commands.Cog):
         await user.send(embed=embed)
         await ctx.channel.purge(limit=1)
         await ctx.send(f'Delivered to {user}', delete_after=6)
-      
 
-  @dm.error
+  @anondm.error
   async def dm_error(self, ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-      await ctx.reply("You need to put a message to send. but you should've known that already dumbass")
+      await ctx.reply("You need to put a message to send. but that should've been a given")
   
   
-  @dm.error
+  @anondm.error
   async def validuser_error(self, ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-      await ctx.reply("You need to say a valid user to dm. but i thought that was obvious")
-      
+      await ctx.reply("You need to say a valid user to dm. but i assumed that you knew that already")
+
   @commands.command()
   async def feedfish(self, ctx):
     embed = nextcord.Embed(title="Yummy :fish:", description="You fed bingus some fish and Bingus starts to pur.")
