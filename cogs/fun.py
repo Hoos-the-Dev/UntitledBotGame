0246c404966c2cc145dbc75a7e0eb0032beb5108
Aurora Rpg <supadj223@gmail.com>
2022-03-24 13:33:17 +0000

    bebebbe

--- cogs/fun.py
+++ cogs/fun.py
@@ -67,7 +67,7 @@ class Fun(commands.Cog):
     async with aiohttp.ClientSession() as cd:
       async with cd.get("https://www.reddit.com/r/animememes.json") as r:
         animememes = await r.json()
-        embed = nextcord.Embed(title="Here ya go bud", description="heres an anime meme ig", color=nextcord.Color.random())
+        embed = nextcord.Embed(title="Here ya go bud", description="heres an anime meme ig")
         embed.set_image(url=animememes["data"]["children"][random.randit(0, 30)]["data"]["url"])
         embed.set_footer("fucking weeb")
         await ctx.reply(embed=embed)
