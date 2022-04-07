10f1afa8f84983e38c1acf46e93d60982d4d125c
Aurora Rpg <supadj223@gmail.com>
2022-04-07 17:58:02 +0000

    temp version of the blacklist

--- cogs/admin.py
+++ cogs/admin.py
@@ -13,6 +13,10 @@ class Admin(commands.Cog):
   @commands.command()
   @has_permissions(kick_members=True)
   async def kick(self, ctx, member: nextcord.Member, *, reason=None):
+    blacklist = 637997826571239454
+    if ctx.author.id == blacklist:
+      await ctx.reply("why should i. you're blacklisted")
+    else:
       embed = nextcord.Embed(title="Kicked! :boot::boom:", description=f"{member} was kicked from the nextcord")
       await member.kick(reason=reason)
       await ctx.reply(embed=embed)
@@ -34,6 +38,10 @@ class Admin(commands.Cog):
   @commands.command()
   @has_permissions(ban_members=True)
   async def ban(self, ctx, member: nextcord.Member, *, reason=None):
+    blacklist = 637997826571239454
+    if ctx.author.id == blacklist:
+      await ctx.reply("no")
+    else:
       embed = nextcord.Embed(title="Banned! :boot::boom:", description=f"{member} was banned from the discord")
       embed.add_field(name="Reason:", value=reason)
       dembed = nextcord.Embed(title="Uh Oh...", description=f'You were banned from **{ctx.guild.name}')
@@ -52,15 +60,22 @@ class Admin(commands.Cog):
   @commands.command()
   @has_permissions(manage_messages=True)
   async def purge(self, ctx, amount=11):
+      blacklist = 637997826571239454
       amount = amount + 1
+
       if amount < 3:
         purgemin = nextcord.Embed(title='You are trying to purge to little', description='You couldnt delete one message on your own?')
         await ctx.reply(embed=purgemin, delete_after=5)
         return
-      if amount > 101:
+      elif amount > 101:
         purgefail = nextcord.Embed(title="Fail to Purge", description="Cannot remove more than 100 messages")
         await ctx.reply(embed=purgefail, delete_after=5)
-      else:
+        return
+      elif ctx.author.id == blacklist:
+          embed = nextcord.Embed(title="Fuck off.", description=f"if i check my book it says that **{ctx.author.name}** has been blacklisted from using this command.", color=nextcord.Color.red())
+          embed.set_footer(text="how did you fuck up that badly to where you got blacklisted.")
+          await ctx.reply(embed=embed)
+      else: 
         embed = nextcord.Embed(title='Purged!:bomb:', description=f'Deleted {amount-1} messages')
         await ctx.channel.purge(limit=amount)
         await ctx.send(embed=embed, delete_after=5)
@@ -69,7 +84,7 @@ class Admin(commands.Cog):
   async def purge_error(self, ctx, error,):
       if isinstance(error, commands.MissingPermissions):
         purgeerror = nextcord.Embed(title="Missing Permission :x:", description="You dont have the permissions to Purge Messages!")
-      await ctx.reply(embed=purgeerror, delete_after=5)
+      await ctx.reply(embed=purgeerror)
 
 def setup(bot):
     bot.add_cog(Admin(bot))
